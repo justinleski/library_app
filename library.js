fed56371e53bacd56f6834f682bef331fcd4b262
Justin <o0justinsgamez0o@gmail.com>
2024-06-02 17:57:49 -0400

    fix: Include JS src and defer it

--- library.js
+++ library.js
@@ -1,9 +1,32 @@
 const myLibrary = []; // array where books are stored
 
-function Book() { // constructor
+// Bind the action in JS - bad practice to use HTML
 
+document.querySelector("#bookForm").addEventListener("submit", addBook);
+console.log("Hello 2");
+
+function Book(name, author, read) { // constructor
+    this.name = name;
+    this.author = author;
+    this.read = read;
 }
 
 function addBook() {
-    //
+
+    console.log("Hello works");
+
+    // !!! we need to use constructor to make new book
+    let tempBook = document.getElementById("bookForm");
+    tempBook.addEventListener("submit", function(event){
+        event.preventDefault(); // prevents default browser behaviour like pg refresh
+
+
+        //new Book(tempBook); // FormData() is also useful
+
+        // USEFUL: https://www.youtube.com/watch?v=7LGpIQ6ceJs
+    });
+
+    // Add constructor to array
+    //myLibrary.push(tempBook);
 }
+
