21606255dd68c13fe2c45f2ed0b99742e422d361
Justin <o0justinsgamez0o@gmail.com>
2024-06-03 11:25:01 -0400

    fix: Display value of name inputted by user in appropriate field

--- library.js
+++ library.js
@@ -8,20 +8,38 @@ function Book(name, author, read) { // constructor
 
 function updateGrid(){
 
-    for (var book of myLibrary) {
+    for (const book of myLibrary) {
+
+        // Keep in mind - we need a .value in order to print the value of the object
+        // alert(book.author.value);
+
         // Create card
         const bookCard = document.createElement("div");
         bookCard.className = "bookCard";
 
         // Add title
         const bookName = document.createElement("h1");
-        bookName.textContent = book.name;
+        bookName.textContent = book.name.value;
         bookCard.appendChild(bookName);
     
         // Add author
         const bookAuth = document.createElement("h2");
-        bookAuth.textContent = book.author;
+        bookAuth.textContent = book.author.value;
         bookCard.appendChild(bookAuth);
+
+        // Add remove box
+        const removeBox = document.createElement("div");
+        removeBox.style.display = "flex";
+        removeBox.style.justifyContent = "end";
+
+        // Put button in box
+        const remBtn = document.createElement("img");
+        remBtn.setAttribute("src", "imgs/close-round-icon.svg");
+        remBtn.className = "remBtn";
+        
+
+        removeBox.appendChild(remBtn);
+        bookCard.appendChild(removeBox);
 
         // Add card to grid
         document.getElementById("bookGrid").appendChild(bookCard);
@@ -47,6 +65,6 @@ function addBook() {
 
 // Bind the action in JS - bad practice to use HTML
 document.querySelector("#bookForm").addEventListener("submit", function(event){
-    event.preventDefault();
+    event.preventDefault(); // stops page from refreshing automatically
     addBook();
 });
